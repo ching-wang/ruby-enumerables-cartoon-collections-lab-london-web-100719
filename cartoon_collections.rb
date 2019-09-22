213f794aed5a9231cc34185fa8b9bafd22a3ee25
Qing Wang <chingwang.qw@gmail.com>
2019-09-22 14:50:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(names_array)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map {|calls| calls..capitalize + "!"}
+  planeteer_calls.map {|calls| calls.capitalize + "!"}
 end
 
 def long_planeteer_calls(short_words)
