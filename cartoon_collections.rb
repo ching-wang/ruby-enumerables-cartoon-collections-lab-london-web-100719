46da9c35d09f9c6191864925860e172701a07dc5
Qing Wang <chingwang.qw@gmail.com>
2019-09-22 14:30:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(short_words)
-  short_words.include? {|a| a > 4}
+  short_words.find_index {|v, i| }
 end
 
 def find_the_cheese# code an argument here
