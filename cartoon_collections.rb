33ad2aedc17a3e960111efeae53f5ffaa46f1f6a
Qing Wang <chingwang.qw@gmail.com>
2019-09-22 15:09:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,15 @@ def long_planeteer_calls(short_words)
   short_words.any? {|word| word.length > 4}
 end
 
-cheese_types = ["cheddar", "gouda", "camembert"]
+cheese_types = (array)
 def find_the_cheese(cheese_box)
   cheese_box.find {|type| type == "cheddar" && type == "gouda" && "camembert"}
 end
+
+def find_the_cheese(array)
+  cheese_types = ["cheddar", "gouda", "camembert"]
+    array.find do |type|
+    cheese_types.include?(type)
+  end 
+end
+
